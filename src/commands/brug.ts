7d61c7524d72f5aa7f01dddd36e290f43bbd16e1
TimKardol <tnmkardol@hotmail.com>
2020-11-30 16:20:52 +0100

    Add description and new image

--- src/commands/brug.ts
+++ src/commands/brug.ts
@@ -7,7 +7,8 @@ module.exports = {
         message.channel.send(new MessageEmbed()
         .setColor('007fff')
         .setTitle('Mooie brug')
-        .setImage('https://upload.wikimedia.org/wikipedia/commons/a/a1/Zeelandbrug.jpg')
+        .setDescription('The Zeeland Bridge is the longest bridge in the Netherlands. The bridge spans the Oosterschelde estuary. It connects the islands of Schouwen-Duiveland and Noord-Beveland in the province of Zeeland. The Zeeland Bridge was built between 1963 and 1965.')
+        .setImage('https://cdn.discordapp.com/attachments/644496515037003779/769144183420092426/unknown.png')
         )
     },
 };
